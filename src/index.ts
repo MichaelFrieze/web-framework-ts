db569694dce9a8e8759ed694025ddc890feabe39
Michael Frieze <michaelfrieze@icloud.com>
2021-09-29 17:16:13 -0400

    ooptional interface properties

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,31 @@ console.log(
 
 // user.get('age');
 console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
+
+console.log('setting new values');
+
+user.set({ name: 'John Adams', age: 9999 });
+
+// user.get('name');
+console.log(
+  "🚀 ~ file: index.ts ~ line 6 ~ user.get('name')",
+  user.get('name')
+);
+
+// user.get('age');
+console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
+
+console.log(
+  'after adding optional to the UserProps interface properties, we can set 2, 1, or even 0 values. Also this makes it so that we do not have to initially provide a name and age in the constructor'
+);
+
+user.set({ name: 'Name Only' });
+
+// user.get('name');
+console.log(
+  "🚀 ~ file: index.ts ~ line 6 ~ user.get('name')",
+  user.get('name')
+);
+
+// user.get('age');
+console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
