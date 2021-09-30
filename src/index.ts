9692221c360c2a76115fe08451d82a46b3752eda
Michael Frieze <michaelfrieze@icloud.com>
2021-09-30 12:34:36 -0400

    triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Michael Frieze', age: 33 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('moreChange', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user);
+user.trigger('change');
+user.trigger('save');
+user.trigger('does not work');
