06a7c12db6c2f7c006848a290865de9c32f4db23
Michael Frieze <michaelfrieze@icloud.com>
2021-09-30 12:27:54 -0400

    dynamic array creation

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Michael Frieze', age: 33 });
 
-// user.get('name');
-console.log(
-  "🚀 ~ file: index.ts ~ line 6 ~ user.get('name')",
-  user.get('name')
-);
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('moreChange', () => {});
 
-// user.get('age');
-console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
-
-console.log('setting new values');
-
-user.set({ name: 'John Adams', age: 9999 });
-
-// user.get('name');
-console.log(
-  "🚀 ~ file: index.ts ~ line 6 ~ user.get('name')",
-  user.get('name')
-);
-
-// user.get('age');
-console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
-
-console.log(
-  'after adding optional to the UserProps interface properties, we can set 2, 1, or even 0 values. Also this makes it so that we do not have to initially provide a name and age in the constructor'
-);
-
-user.set({ name: 'Name Only' });
-
-// user.get('name');
-console.log(
-  "🚀 ~ file: index.ts ~ line 6 ~ user.get('name')",
-  user.get('name')
-);
-
-// user.get('age');
-console.log("🚀 ~ file: index.ts ~ line 9 ~ user.get('age')", user.get('age'));
+console.log(user);
