8f57816651688a1947c828a8a509c0674070925d
Michael Frieze <michaelfrieze@icloud.com>
2021-10-06 12:56:30 -0400

    connecting sync back to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 
@@ -8,8 +7,11 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
