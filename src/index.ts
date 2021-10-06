0f9d6491b19c4adcc5d2953bf782971be1ac4163
Michael Frieze <michaelfrieze@icloud.com>
2021-10-06 09:40:18 -0400

    using get and post request to make sure server is working

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Michael Frieze', age: 33 });
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20,
+// });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered');
-});
-
-user.trigger('change');
-user.trigger('save');
-user.trigger('does not work');
+axios.get('http://localhost:3000/users/1');
