3b0768bcb8254dfea8bed7a1eba5968776fb7adf
Michael Frieze <michaelfrieze@icloud.com>
2021-10-13 16:18:10 -0400

    passthrough methods: simplified the get accessor to create a methods passthrough

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 77 });
 
-// using a get accessor to assign a reference to the on() method in the eventing class
-const on = user.on;
+// // using a get accessor to assign a reference to the on() method in the eventing class
+// const on = user.on;
 
-// for example:
-// also, calling on() needs an event name and a callback
-on('change', () => {
+// // for example:
+// // also, calling on() needs an event name and a callback
+// on('change', () => {
+//   console.log('user changed');
+// });
+
+// to simplify this:
+user.on('change', () => {
   console.log('user changed');
 });
