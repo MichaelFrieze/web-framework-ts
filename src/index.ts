2b5dfe1eec5936d12a9d78f45b7da1b3c5e15dbe
Michael Frieze <michaelfrieze@icloud.com>
2021-10-13 16:41:33 -0400

    context issue solved with arrow functions

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,11 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 77 });
 
-// // using a get accessor to assign a reference to the on() method in the eventing class
-// const on = user.on;
+// only works if get method in Attributes is an arrow function because of 'this'
+console.log(user.get('name'));
 
-// // for example:
-// // also, calling on() needs an event name and a callback
-// on('change', () => {
-//   console.log('user changed');
-// });
-
-// to simplify this:
 user.on('change', () => {
   console.log('user changed');
 });
+
+user.trigger('change');
--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -3,22 +3,19 @@ import { UserProps } from './User';
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  // get<K extends keyof T>(key: K): T[K] {
+  //   return this.data[key];
+  // }
+
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
-  set(update: T): void {
+  // set(update: T): void {
+  //   Object.assign(this.data, update);
+  // }
+
+  set = (update: T): void => {
     Object.assign(this.data, update);
-  }
+  };
 }
-
-// const attrs = new Attributes<UserProps>({
-//   id: 5,
-//   name: 'asdf',
-//   age: 20,
-// });
-
-// // now we can know the types of each property of the object
-// const name = attrs.get('name');
-// const age = attrs.get('age');
-// const id = attrs.get('id');
